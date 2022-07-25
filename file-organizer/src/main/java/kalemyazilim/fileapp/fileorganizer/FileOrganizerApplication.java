72682687ac6d4de2dfe3a83ae9641ccb1ee83422
omeremksz <omeremksz@gmail.com>
2022-07-25 14:47:45 +0300

    Initial Project Add.

---
+++ file-organizer/src/main/java/kalemyazilim/fileapp/fileorganizer/FileOrganizerApplication.java
@@ -0,0 +1,13 @@
+package kalemyazilim.fileapp.fileorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileOrganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileOrganizerApplication.class, args);
+    }
+
+}
